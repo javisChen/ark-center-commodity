807375f0a992e4f0c51d5e347000702a25845387
Javis <javischen9548@foxmail.com>
2022-02-25 18:22:51 +0800

    u

--- kt-cloud-commodity-start/src/main/java/com/kt/cloud/commodity/Application.java
+++ kt-cloud-commodity-start/src/main/java/com/kt/cloud/commodity/Application.java
@@ -1,14 +1,17 @@
 package com.kt.cloud.commodity;
 
+import com.kt.component.web.config.CloudAppConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@MapperScan(basePackages = "com.kt.cloud.dao")
+@MapperScan(basePackages = "com.kt.cloud.commodity.dao")
 @SpringBootApplication
+@EnableFeignClients(basePackages = {})
 @EnableDiscoveryClient
-public class Application {
+public class Application extends CloudAppConfig {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
