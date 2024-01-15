01a6f987c34355fad9ddf1ff099d19254fe81e21
javisChen <javischen9548@foxmail.com>
2024-01-15 23:11:59 +0800

    u

--- ark-center-product-client/src/main/java/com/ark/center/product/client/search/dto/SearchResultDTO.java
+++ ark-center-product-client/src/main/java/com/ark/center/product/client/search/dto/SearchResultDTO.java
@@ -1,16 +1,13 @@
 package com.ark.center.product.client.search.dto;
 
 import lombok.Data;
-import org.apache.commons.collections4.CollectionUtils;
 
-import java.util.Collections;
 import java.util.List;
 
 @Data
 public class SearchResultDTO {
 
-    private List<SkuSearchDTO> skus = Collections.emptyList();
+    private List<SkuSearchDTO> skus;
 
-    private List<AggDTO> agg = Collections.emptyList();
-
+    private List<AggDTO> agg;
 }
