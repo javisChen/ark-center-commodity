86b86036eaf70d9f7c5f1e28015d1a74ffdc3d9c
chengmo.cjw <javischen9548@foxmail.com>
2025-04-02 18:16:15 +0800

    u

--- ark-center-product-adapter/src/main/java/com/ark/center/member/adapter/member/web/MemberController.java
+++ ark-center-product-adapter/src/main/java/com/ark/center/member/adapter/member/web/MemberController.java
@@ -39,44 +39,4 @@ public class MemberController implements MemberCommandApi, MemberQueryApi {
         return SingleResponse.ok(memberAuthDTO);
     }
     
-    @Override
-    public SingleResponse<MemberAuthDTO> getOrRegisterByMobile(String mobile) {
-        log.info("开始获取或注册会员: mobile={}", mobile);
-        
-        // 先尝试获取会员信息
-        MemberAuthDTO memberAuthDTO = memberAuthService.getMemberAuthByIdentity(mobile);
-        
-        // 如果会员不存在，则自动注册
-        if (memberAuthDTO == null) {
-            log.info("会员不存在，准备自动注册: mobile={}", mobile);
-            
-            // 调用注册接口
-            MemberRegisterDTO registerDTO = registerByMobile(mobile);
-            log.info("会员自动注册成功: mobile={}, memberId={}", mobile, registerDTO.getMemberId());
-            
-            // 获取新注册会员的认证信息
-            memberAuthDTO = memberAuthService.getMemberAuthByIdentity(mobile);
-        } else {
-            log.info("会员已存在: mobile={}, memberId={}", mobile, memberAuthDTO.getMemberId());
-        }
-        
-        return SingleResponse.ok(memberAuthDTO);
-    }
-    
-    /**
-     * 通过手机号注册会员
-     * 
-     * @param mobile 手机号
-     * @return 注册结果
-     */
-    private MemberRegisterDTO registerByMobile(String mobile) {
-        // 创建注册命令
-        MemberRegisterCommand command = new MemberRegisterCommand();
-        command.setRegisterType(RegisterType.MOBILE);
-        command.setMobile(mobile);
-        command.setRegisterChannel("OFFICIAL"); // 使用官方渠道
-        
-        // 调用现有的注册方法
-        return memberCommandHandler.register(command);
-    }
-} 
+}
